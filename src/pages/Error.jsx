20993285ff3f9633234e1862177ecc2154f759ab
steiner112 <asvmusic26@gmail.com>
2024-09-14 12:34:23 +0530

    fix all errors 404

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React from 'react';
+import Lost from "../assets/lost.png"
 
 export default function Error() {
   return (
-    <main>
+    <main style={{flexDirection : "column"}}>
+      <img style={{width: "30vw",minWidth: "300px"}} src={Lost} alt="lst" />
       <h1>ERROR 404</h1>
       <p>Page not Found...</p>
     </main>
